833761d9c9dceb040515b5e6efbe520afdb1345c
soumyajit9979 <soumyajitcyberprotection1405@gmail.com>
2022-12-13 22:47:10 +0530

    naya file add kiya, aur naya function(ANGLE) used

--- MQTT/subscriber.py
+++ MQTT/subscriber.py
@@ -48,6 +48,28 @@ def control_bot(client,userdata,message):
                 cv2.imshow("input", frame)
                 if cv2.waitKey(1) == 113:
                     break
+
+            
+        cv2.destroyAllWindows()
+
+    
+    elif(input == "ALIGN_RABBIT"):
+        VideoCap = True
+        capture = cv2.VideoCapture(0)
+        while True:
+            if VideoCap:
+                _, frame = capture.read()
+                # frame = cv2.resize(frame, (0, 0), fx=0.7, fy=0.7)
+                ARUCO_DICT, ARUCO_PARAMS = findAruco(frame)
+                distance_pose(frame, ARUCO_DICT=ARUCO_DICT, ARUCO_PARAMS=ARUCO_PARAMS)
+                if ID() > -1:
+                    a=ANGLE()
+                    PID(a)
+                cv2.imshow("input2", frame)
+                if cv2.waitKey(1) == 113:
+                    break
+
+            
         cv2.destroyAllWindows()
         
 
