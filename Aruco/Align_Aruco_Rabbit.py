014ef04ec0860bb83000677b4c5a0235f367486d
Malay <malaykumar2003@gmail.com>
2022-12-14 20:36:38 +0530

    Rabbit Commented

--- Aruco/Align_Aruco_Rabbit.py
+++ Aruco/Align_Aruco_Rabbit.py
@@ -1,41 +1,41 @@
-import sys
-sys.path.append("Motors")
-from RabbitMotor import *
+# import sys
+# sys.path.append("Motors")
+# from RabbitMotor import *
 
-kp = 15
-kd = 20
-ki = 0.02
+# kp = 15
+# kd = 20
+# ki = 0.02
 
-priError = 0
-toError = 0
-
-
-def mapping(value, x, y, a, b):
-    res = abs(b-a)/(y-x)
-    return res*value
+# priError = 0
+# toError = 0
 
 
-def PID_rabbit(x):
-    print("Aligning rabbit")
-    global priError
-    global toError
-    if x != None:
-        curr_x = x
-        set_x = 0
-        error = set_x - curr_x
-        Pvalue = error * kp
-        Ivalue = toError * ki
-        Dvalue = (error - priError) * kd
-        PIDvalue = Pvalue + Ivalue + Dvalue
-        priError = error
-        toError = error + toError
-        print(PIDvalue)
-        res_val = mapping(PIDvalue, 0, 60, 0, 100)
-        if PIDvalue > 0:
-            # LEFT
-            rabitRotateL(res_val)
-        elif PIDvalue < 0:
-            # RIGHT
-            rabitRotateR(res_val)
-        else:
-            rlstop()
+# def mapping(value, x, y, a, b):
+#     res = abs(b-a)/(y-x)
+#     return res*value
+
+
+# def PID_rabbit(x):
+#     print("Aligning rabbit")
+#     global priError
+#     global toError
+#     if x != None:
+#         curr_x = x
+#         set_x = 0
+#         error = set_x - curr_x
+#         Pvalue = error * kp
+#         Ivalue = toError * ki
+#         Dvalue = (error - priError) * kd
+#         PIDvalue = Pvalue + Ivalue + Dvalue
+#         priError = error
+#         toError = error + toError
+#         print(PIDvalue)
+#         res_val = mapping(PIDvalue, 0, 60, 0, 100)
+#         if PIDvalue > 0:
+#             # LEFT
+#             rabitRotateL(res_val)
+#         elif PIDvalue < 0:
+#             # RIGHT
+#             rabitRotateR(res_val)
+#         else:
+#             rlstop()
--- MQTT/publisher.py
+++ MQTT/publisher.py
@@ -54,45 +54,45 @@ def Controller_Elephant():
                     return "STOP-"+"0"
             return 0
 
-def Controller_Rabbit():
-    while True:
-        for event in pygame.event.get():
-            print(event)
-            if event.type == pygame.JOYBUTTONDOWN:
-                if pygame.joystick.Joystick(0).get_button(5):
-                    return "ALIGN_ARUCO_ELEPHANT"
-                if pygame.joystick.Joystick(0).get_button(6):
-                    return "STOP_EVENT"
-                if pygame.joystick.Joystick(0).get_button(7):
-                    return "STOP-0"
-            if event.type == pygame.JOYAXISMOTION:
-                AXIS_0 = pygame.joystick.Joystick(0).get_axis(0)
-                # Left - Right
-                AXIS_1 = pygame.joystick.Joystick(0).get_axis(1)
-                # Forward - Backward
-                AXIS_4 = pygame.joystick.Joystick(0).get_axis(4)
-                # Rotate Left
-                AXIS_5 = pygame.joystick.Joystick(0).get_axis(5)
-                # Rotate Right
-                if event.__dict__.get("axis") == 1:
-                    if AXIS_1 < 0 and AXIS_1 >= -1:
-                        return "FORWARD-" + str(throttle(AXIS_1))
-                    else:
-                        return "BACKWARD-" + str(throttle(AXIS_1))
-                elif event.__dict__.get("axis") == 0:
-                    if AXIS_0 < 0 and AXIS_0 >= -1:
-                        return "LEFT-" + str(throttle(AXIS_0))
-                    else:
-                        return "RIGHT-" + str(throttle(AXIS_0))
-                elif round(AXIS_4) == -1 and round(AXIS_5) == -1:
-                    return "STOP-0"
-                elif event.__dict__.get("axis") == 4:
-                    return "ROTATE_LEFT-" + str(throttle(AXIS_4))
-                elif event.__dict__.get("axis") == 5:
-                    return "ROTATE_RIGHT-" + str(throttle(AXIS_5))
-                else:
-                    return "STOP-"+"0"
-            return 0
+# def Controller_Rabbit():
+#     while True:
+#         for event in pygame.event.get():
+#             print(event)
+#             if event.type == pygame.JOYBUTTONDOWN:
+#                 if pygame.joystick.Joystick(0).get_button(5):
+#                     return "ALIGN_ARUCO_ELEPHANT"
+#                 if pygame.joystick.Joystick(0).get_button(6):
+#                     return "STOP_EVENT"
+#                 if pygame.joystick.Joystick(0).get_button(7):
+#                     return "STOP-0"
+#             if event.type == pygame.JOYAXISMOTION:
+#                 AXIS_0 = pygame.joystick.Joystick(0).get_axis(0)
+#                 # Left - Right
+#                 AXIS_1 = pygame.joystick.Joystick(0).get_axis(1)
+#                 # Forward - Backward
+#                 AXIS_4 = pygame.joystick.Joystick(0).get_axis(4)
+#                 # Rotate Left
+#                 AXIS_5 = pygame.joystick.Joystick(0).get_axis(5)
+#                 # Rotate Right
+#                 if event.__dict__.get("axis") == 1:
+#                     if AXIS_1 < 0 and AXIS_1 >= -1:
+#                         return "FORWARD-" + str(throttle(AXIS_1))
+#                     else:
+#                         return "BACKWARD-" + str(throttle(AXIS_1))
+#                 elif event.__dict__.get("axis") == 0:
+#                     if AXIS_0 < 0 and AXIS_0 >= -1:
+#                         return "LEFT-" + str(throttle(AXIS_0))
+#                     else:
+#                         return "RIGHT-" + str(throttle(AXIS_0))
+#                 elif round(AXIS_4) == -1 and round(AXIS_5) == -1:
+#                     return "STOP-0"
+#                 elif event.__dict__.get("axis") == 4:
+#                     return "ROTATE_LEFT-" + str(throttle(AXIS_4))
+#                 elif event.__dict__.get("axis") == 5:
+#                     return "ROTATE_RIGHT-" + str(throttle(AXIS_5))
+#                 else:
+#                     return "STOP-"+"0"
+#             return 0
 
 
 def MOVE_Bot_1():
@@ -101,11 +101,11 @@ def MOVE_Bot_1():
         print(Value)
         client.publish("CONTROLLER_INPUTS_ELEPHANT", Value)
 
-def MOVE_Bot_2():
-    Value = Controller_Rabbit()
-    if Value != 0:
-        print(Value)
-        client.publish("CONTROLLER_INPUTS_RABBIT", Value)
+# def MOVE_Bot_2():
+#     Value = Controller_Rabbit()
+#     if Value != 0:
+#         print(Value)
+#         client.publish("CONTROLLER_INPUTS_RABBIT", Value)
 
 
 client_id = "Controller"
@@ -119,4 +119,4 @@ if client.connect(broker, port) != 0:
 
 while True:
     MOVE_Bot_1()
-    MOVE_Bot_2()
+    # MOVE_Bot_2()
